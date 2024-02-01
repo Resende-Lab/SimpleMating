d80ad31d043a9c7611876e4540b41633c49e64ab
Marco Antônio Peixoto <59318360+marcopxt@users.noreply.github.com>
2024-02-01 09:05:22 -0500

    Update and rename generic_pedigree.R to generic_Pedigree.R

--- R/generic_Pedigree.R
+++ R/generic_Pedigree.R
@@ -1,5 +1,5 @@
 #' @title generic_Pedigree
-#' @description A pedigree information for 100 individuals. The three columns represents the individuals, mother, and father identification, respectively.
+#' @description A pedigree information for 100 individuals. The three columns represent the individual, mother, and father identification.
 #' @format A data frame with 120 rows and 3 variables.
 #' \describe{
 #'   \item{\code{id}}{Individuals' id}
